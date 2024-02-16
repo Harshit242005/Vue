8800bf995f5946160f45761d326c4e772dbc3865
Harshit242005 <agreharshit610@gmail.com>
2024-02-16 22:21:09 +0530

    Learning to integrate tailwind css in the VUE application and creating initial styles

---
+++ my-project/src/App.vue
@@ -0,0 +1,39 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="flex flex-row justify-center items-center" style="gap: 800px;">
+    <p class="Heading">Reachbox</p>
+
+    <button class="w-48 h-16 hover:bg-cyan-300 flex items-center justify-center gap-4 border-2
+    bg-white
+    shadow-lg
+    text-black text-2xl hover:text-white font-semibold
+    mt-4
+     border-cyan-300 rounded-sm">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"
+        class="w-8 h-8">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+      </svg>
+
+       Create
+    </button>
+
+
+  </div>
+</template>
+
+<style scoped>
+.Heading {
+  font-size: 40px;
+  text-align: left;
+  padding-left: 25px;
+  padding-top: 10px;
+  font-weight: 600;
+  background: linear-gradient(to right, rgba(246, 175, 175, 0.73), rgba(0, 0, 0, 0.652), rgba(53, 255, 219, 0.23));
+  -webkit-text-fill-color: transparent;
+  -webkit-background-clip: text;
+
+}
+</style>
